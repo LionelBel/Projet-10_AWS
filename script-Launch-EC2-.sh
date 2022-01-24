0fe9f07122065cc0423fa0cedadec44c951838ea
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2022-01-24 11:03:54 +0100

    Personnalisation du script ( Instance vierge).

--- script-Launch-EC2-.sh
+++ script-Launch-EC2-.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# L'image réalisée a Apache2 et Docker d'installé
-# Désactivation du service httpd
-sudo systemctl stop httpd.service
-sudo systemctl status httpd.service
-sudo systemctl disable httpd.service
-# Demarrage du service docker
+## Installation et configuration de Docker sur instance vierge Amazon Linux 2 AMI (HVM)
+sudo yum update -y
+sudo amazon-linux-extras install docker
+       ########################
+### Configuration image réalisée avec Apache2 et Docker d'installé ###
+## Désactivation du service httpd
+#sudo systemctl stop httpd.service
+#sudo systemctl status httpd.service
+#sudo systemctl disable httpd.service
+       #######################
+## Demarrage du service docker
+sudo systemctl enable docker
 sudo systemctl start docker.service
-sudo systemctl status docker.service
-# Mise en place de notre container WordPress personnalisé
+#sudo systemctl status docker.service
+## Mise en place de notre container WordPress personnalisé
 docker pull lionelbel/wordpress-custom
-docker images -a
+#docker images -a
 docker run --name=wordpress -d -p 80:80 lionelbel/wordpress-custom:latest 
-docker exec wordpress -it /var/www/Install-WP.sh
+## Excécution du script de configuration du site 
+docker exec wordpress -it /var/www/install-site-wp.sh 
