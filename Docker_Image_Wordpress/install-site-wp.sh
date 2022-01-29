dd1415c913a3cd4890e3ded4af4a183d37906c59
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2022-01-29 10:04:08 +0100

    Modification du parametre --url= .

--- Docker_Image_Wordpress/install-site-wp.sh
+++ Docker_Image_Wordpress/install-site-wp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configuration du site Wordpress
-wp core install --allow-root --path=/var/www/html --url=Projet10.lab --title=Maîtrisez_votre_infrastructure_cloud --admin_user=Lionel --admin_password=Lionel@77 --admin_email=lionel@bellerose.ovh
+wp core install --allow-root --path=/var/www/html --url=Lionel-openclassrooms-2022.eu-west-3.elb.amazonaws.com --title=Maîtrisez_votre_infrastructure_cloud --admin_user=Lionel --admin_password=Lionel@77 --admin_email=lionel@bellerose.ovh
 
 # Installation du plugin WP Oflload Media
 wp plugin install amazon-s3-and-cloudfront --allow-root --path=/var/www/html --activate
