8b1e4ce6cc5d8ca0e968822718ce9564dcb5f5d6
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2022-02-05 14:30:46 +0100

    Ajout description du script.

--- Publish_image_DockerHUB.bat
+++ Publish_image_DockerHUB.bat
@@ -1,3 +1,5 @@
+## Script de construction de l'image et sa publication dans DockerHub
+
 docker build -t wordpress-custom ./Docker_Image_Wordpress
 docker tag wordpress-custom:latest lionelbel/wordpress-custom:latest
 docker push lionelbel/wordpress-custom:latest
