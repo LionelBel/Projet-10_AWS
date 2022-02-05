8758367b5975a031e5eed98aa7bd11b722e01c92
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2022-02-05 14:25:59 +0100

    Modification du script et intégration template.

--- script-Launch-EC2-.sh
+++ script-Launch-EC2-.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-
-## Installation et configuration de Docker sur instance vierge Amazon Linux 2 AMI (HVM)
-sudo yum update -y
-sudo amazon-linux-extras install docker
-       ########################
-### Configuration image réalisée avec Apache2 et Docker d'installé ###
-## Désactivation du service httpd
-#sudo systemctl stop httpd.service
-#sudo systemctl status httpd.service
-#sudo systemctl disable httpd.service
+### Installation et configuration de Docker sur instance vierge Amazon Linux 2 AMI (HVM)
+yum update -y
+amazon-linux-extras install -y docker 
        #######################
 ## Demarrage du service docker
-sudo systemctl enable docker
-sudo systemctl start docker.service
-#sudo systemctl status docker.service
+systemctl enable docker
+systemctl start docker.service
 ## Mise en place de notre container WordPress personnalisé
 docker pull lionelbel/wordpress-custom
-#docker images -a
-docker run --name=wordpress -d -p 80:80 lionelbel/wordpress-custom:latest 
+docker run --name=wordpress -d -p 80:80 -e WORDPRESS_DB_HOST=${rdshostname} lionelbel/wordpress-custom:latest
 ## Excécution du script de configuration du site 
-docker exec wordpress -it /var/www/install-site-wp.sh 
+docker exec wordpress /var/www/install-site-wp.sh ${lbdnsname}
+## Edition des variables
+rdshostname: !GetAtt WordpressDB.Endpoint.Address
+lbdnsname: !GetAtt WordpressLoadBalancer.DNSName
+
+###############Template###############
+#UserData:
+#        Fn::Base64: !Sub
+#          - |
+#            #!/bin/bash
+#            yum update -y
+#            amazon-linux-extras install docker -y
+#            systemctl enable docker
+#            systemctl start docker.service
+#            docker pull lionelbel/wordpress-custom
+#            docker run --name=wordpress -d -p 80:80 -e WORDPRESS_DB_HOST=${rdshostname} lionelbel/wordpress-custom:latest
+#            docker exec wordpress /var/www/install-site-wp.sh ${lbdnsname}
+#          - rdshostname: !GetAtt WordpressDB.Endpoint.Address
+#            lbdnsname: !GetAtt WordpressLoadBalancer.DNSName
+#
+########################################
