486fc6a771fb8153f153444bec47520b3e5c0a79
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2022-02-05 17:38:39 +0100

    Ajoue de la configuration de la langue Française.

--- Docker_Image_Wordpress/install-site-wp.sh
+++ Docker_Image_Wordpress/install-site-wp.sh
@@ -3,5 +3,8 @@
 # Configuration du site Wordpress
 wp core install --allow-root --path=/var/www/html --url=$1 --title=Maîtrisez_votre_infrastructure_cloud --admin_user=Lionel --admin_password=Lionel@77 --admin_email=lionel@bellerose.ovh
 
+# Configuration de la langue Française
+wp language core activate fr_FR
+
 # Installation du plugin WP Oflload Media
 wp plugin install amazon-s3-and-cloudfront --allow-root --path=/var/www/html --activate
