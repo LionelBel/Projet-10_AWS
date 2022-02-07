8378391034b868137dbcd2e14d9d30c0860eae1d
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2022-02-07 08:37:50 +0100

    Ajout installation de la langue Francaise.

--- Docker_Image_Wordpress/install-site-wp.sh
+++ Docker_Image_Wordpress/install-site-wp.sh
@@ -4,6 +4,7 @@
 wp core install --allow-root --path=/var/www/html --url=$1 --title=Maîtrisez_votre_infrastructure_cloud --admin_user=Lionel --admin_password=Lionel@77 --admin_email=lionel@bellerose.ovh
 
 # Configuration de la langue Française
+wp language core install fr_FR
 wp language core activate fr_FR
 
 # Configuration ddu thème Twenty Twenty
